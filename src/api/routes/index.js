18f6d3abf6a4953af6a13679b675ad22b6863c4a
Fábio Senra <fabio-senra@hotmail.com>
2020-05-06 23:09:49 +0100

    issn started

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,28 +2,15 @@ var express = require('express');
 var router = express.Router();
 var Users = require('../controllers/users')
 var fs = require('fs');
+var axios = require('axios')
+
+var apikey="35aa4d6f60c2873044eb2bcfbc50cb5e"
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   Users.getUsers()
     .then(result => res.render('index', {lista: result}))
     .catch(e => res.status(500).jsonp(e))
-});
-
-router.get('/:idUser', async function(req, res, next) {
-  idUser = req.params.idUser
-  
-  if (req.query.idPub) {
-    Users.getPub(idUser, req.query.idPub)
-      .then(result => res.render('pubInfo', {idUser: idUser, pub: result.publicacoes[0]}))
-      .catch(e => res.status(500).jsonp(e))
-  }
-  
-  else {
-    Users.getUser(idUser)
-      .then(result => res.render('userInfo', {user: result}))
-      .catch(e => res.status(500).jsonp(e)) 
-  }
 });
 
 router.get('/img_avatar.png', function(req, res, next) {
@@ -43,5 +30,33 @@ router.get('/back2.jpg', function(req, res, next) {
     res.end(img);
   })
 });
+
+//ISSN
+
+router.get('/issn/:code', function(req,res,next){
+  console.log("https://api.elsevier.com/content/serial/title/issn/"+ req.params.code+"?apiKey="+apikey)
+  axios.get("https://api.elsevier.com/content/serial/title/issn/"+ req.params.code+"?apiKey="+apikey)
+    .then(dados => res.jsonp(dados.data))
+    //.then(dados => res.render('issn',{issn:dados.data}))
+    .catch(e => res.jsonp(e))
+})
+
+router.get('/:idUser', async function(req, res, next) {
+  idUser = req.params.idUser
+  
+  if (req.query.idPub) {
+    Users.getPub(idUser, req.query.idPub)
+      .then(result => res.render('pubInfo', {idUser: idUser, pub: result.publicacoes[0]}))
+      .catch(e => res.status(500).jsonp(e))
+  }
+  
+  else {
+    Users.getUser(idUser)
+      .then(result => res.render('userInfo', {user: result}))
+      .catch(e => res.status(500).jsonp(e)) 
+  }
+});
+
+
 
 module.exports = router;
