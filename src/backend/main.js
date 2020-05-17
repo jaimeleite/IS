1627f04c27b1eff63cb3e4888fe4fc0ba940140a
NunoRei <36609841+NunoRei@users.noreply.github.com>
2020-05-17 17:34:10 +0100

    tabela ORCIDS para permitir posterior procura de mais orcids

--- src/backend/main.js
+++ src/backend/main.js
@@ -1,14 +1,27 @@
 var Utils = require('./utils/utils')
 var Users = require('./controllers/users')
+var Orcids = require('./controllers/orcids')
 var Connbd = require('./connBD/connBD')
 
 getUsersId = async() => { 
     Connbd.establishConnection('is')
 
-    var users = await Users.getUsers()
+    try {
+        Orcids.insert('0000-0001-6457-0756')
+        Orcids.insert('0000-0003-4121-6169')
+        Orcids.insert('0000-0001-9710-847X')
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+    var users = await Orcids.getORCIDS()
     
-    for(let i = 0; i<users.length; i++){
+    for(let i = 0; i<users.length; i++)
+    {
+        
         await Utils.userInfo(users[i]._id)
+    
     }
   
     Connbd.closeConnection()
